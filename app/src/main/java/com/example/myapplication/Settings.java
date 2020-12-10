a61cdda826e091dd3be509397d7eb9a69d278a7d
klitna <cf19iryna.klitna@iesjoandaustria.org>
2020-12-11 00:44:59 +0100

    language working

--- app/src/main/java/com/example/myapplication/Settings.java
+++ app/src/main/java/com/example/myapplication/Settings.java
@@ -1,23 +1,27 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.example.myapplication.DB.IncidenciaDBHelper;
 
 import java.util.Locale;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 public class Settings extends Fragment {
@@ -31,27 +35,72 @@ public class Settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View fSettings = inflater.inflate(R.layout.fragment_settings, container, false);
+        final Context context = fSettings.getContext();
         final Configuration config = new Configuration(getResources().getConfiguration());
         dbHelper = new IncidenciaDBHelper(container.getContext());
         db = dbHelper.getWritableDatabase();
-        ImageButton toEsButton = fSettings.findViewById(R.id.buttonSpanishFlag);
-        toEsButton.setOnClickListener(new View.OnClickListener() {
+
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+
+        final Button changeLang = fSettings.findViewById(R.id.changeLanguageButton);
+        changeLang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                config.setLocale(esLocale);
-                Toast.makeText(getActivity(), "El idioma se ha cambiado a castellano.", Toast.LENGTH_LONG).show();
+                showChangeLanguageDialog(context);
             }
         });
-
-        ImageButton toEnButton = fSettings.findViewById(R.id.buttonEnglishFlag);
-        toEsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                config.setLocale(enLocale);
-                Toast.makeText(getActivity(), "Language switched to English.", Toast.LENGTH_LONG).show();
-            }
-        });
-
         return fSettings;
+    }
+
+    public void showChangeLanguageDialog(Context context)
+    {
+        final String[] listLanguages = {"English", "Español"};
+        final AlertDialog.Builder dialogLanguages = new AlertDialog.Builder(context);
+        dialogLanguages.setTitle("Choose your language: ");
+        dialogLanguages.setSingleChoiceItems(listLanguages, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which==0)
+                {
+                    //English
+                    setLocale("en");
+                    refresh();
+                }else if(which==1)
+                {
+                    //Spanish
+                    setLocale("es");
+                    refresh();
+                }
+                dialog.dismiss();
+            }
+        });
+        AlertDialog mDialog = dialogLanguages.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getActivity().getBaseContext().getResources().updateConfiguration(config, getActivity().getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = (SharedPreferences.Editor) getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences prefs = getActivity().getSharedPreferences("Settings", MODE_PRIVATE);
+        String language = prefs.getString("My_lang", "");
+        setLocale(language);
+    }
+
+    public void refresh(){
+        getFragmentManager()
+                .beginTransaction()
+                .detach(Settings.this)
+                .attach(Settings.this)
+                .commit();
     }
 }
